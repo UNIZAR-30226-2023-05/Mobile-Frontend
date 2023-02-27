88e74e76c9644f81a0579638157c3140757ff702
pablolftw <779739@unizar.es>
2023-02-27 19:09:32 +0100

    horas

--- oca_app/lib/main.dart
+++ oca_app/lib/main.dart
@@ -81,12 +81,12 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.amber,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Left(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
